cdda74acc040d6c2825ae6e15eb4a48d62e0030c
atoth3 <adam2toth@gmail.com>
2017-10-08 20:57:34 +0200

    Setup NUnit

---
+++ OxygenClean_Test/BasicTests.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace OxygenClean_Test
+{
+    class BasicTests
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void openPage()
+        {
+            driver.Url = "http://www.oxygenclean.hu/";
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+
+    }
+}
