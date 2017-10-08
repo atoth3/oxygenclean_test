398604b44fbb2ab80d1c01ff6a5f099b70308944
atoth3 <adam2toth@gmail.com>
2017-10-08 20:23:54 +0200

    Begining

---
+++ OxygenClean_Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+
+namespace OxygenClean_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Url = "http://www.oxygenclean.hu/";
+        }
+    }
+}
