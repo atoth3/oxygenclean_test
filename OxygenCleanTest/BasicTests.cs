b4226977c0b7b8cc98baa6cefd53c5a62b92b35e
atoth3 <adam2toth@gmail.com>
2017-10-11 21:00:40 +0200

    modificaton

---
+++ OxygenCleanTest/BasicTests.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace OxygenCleanTest
+{
+    public class BasicTests
+    {
+        IWebDriver driver;
+        ChromeOptions options = new ChromeOptions();
+
+        [SetUp]
+        public void Initialize()
+        {
+            options.AddArgument("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
+        [Test]
+        public void BackToMainPage()
+        {
+            driver.Url = "http://www.oxygenclean.hu/";
+
+            driver.FindElement(By.XPath("//A[@href='http://www.oxygenclean.hu/kapcsolat/'][text()='Kapcsolat']"))
+                .Click();
+
+            driver.Navigate().Back();
+            string title = driver.Title;
+            Assert.AreEqual(title, "Oxygen Clean");
+        }
+        [Test]
+        public void ClickToContact()
+        {
+            driver.Url = "http://www.oxygenclean.hu/";
+
+            By Locator = By.XPath("//A[@href='http://www.oxygenclean.hu/ajanlatot-kerek/'][text()='Ajánlatot kérek']");
+
+            IWebElement askOffer = driver.FindElement(Locator);
+
+            askOffer.Click();
+
+            IWebElement name = driver.FindElement(By.XPath("//INPUT[@id='ninja_forms_field_25']"));
+
+            name.Submit();
+
+            string title = driver.Title;
+            Assert.AreEqual(title, "Ajánlatot kérek | Oxygen Clean");
+        }
+        [Test]
+        public void openPage()
+        {
+            driver.Url = "http://www.oxygenclean.hu/";
+            string title = driver.Title;
+            Assert.AreEqual(title, "Oxygen Clean");
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
